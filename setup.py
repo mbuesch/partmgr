adc58000bf56bd76c73ad97c0d84bea54609dfa7
Michael Buesch <m@bues.ch>
2015-12-23 17:31:26 +0100

    Update setup script
    Signed-off-by: Michael Buesch <m@bues.ch>

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+from partmgr.core.version import VERSION_STRING
 from distutils.core import setup
 
 
 setup(	name		= "partmgr",
-	version		= "0.0",
+	version		= VERSION_STRING,
 	description	= "Part manager",
 	license		= "GNU General Public License v2 or later",
 	author		= "Michael Buesch",
@@ -12,8 +13,10 @@ setup(	name		= "partmgr",
 	url		= "http://bues.ch/cms/hacking/...",
 	packages	= [ "partmgr",
 			    "partmgr/core",
+			    "partmgr/pricefetch",
 			    "partmgr/gui", ],
-	scripts		= [ "partmgr-gui", ],
+	scripts		= [ "partmgr-gui",
+			    "partmgr-import-partdb", ],
 	keywords	= [ ],
 	classifiers	= [
 	],

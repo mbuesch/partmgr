ba0bfbcc4ebb4404f0b6f2b75846b671239856b3
Michael Buesch <m@bues.ch>
2015-12-23 20:48:19 +0100

    pricefetch: Add Pollin price fetcher
    Signed-off-by: Michael Buesch <m@bues.ch>

--- partmgr/pricefetch/__init__.py
+++ partmgr/pricefetch/__init__.py
@@ -1,2 +1,3 @@
+from .pollin import *
 from .reichelt import *
 
---
+++ partmgr/pricefetch/pollin.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+#
+# PartMgr - Price fetching
+#
+# Copyright 2015 Michael Buesch <m@bues.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from partmgr.pricefetch.pricefetch import *
+
+import re
+import time
+
+
+class PollinPriceFetcher(PriceFetcher):
+	supplierNames = ("Pollin",
+			 "pollin.de",
+			 "www.pollin.de",)
+	host = "www.pollin.de"
+
+	def __init__(self, tls = True):
+		self.__sid = None
+		PriceFetcher.__init__(self, tls = tls)
+
+	def __getSessionId(self):
+		for i in range(5):
+			self._sendRequest("GET", "/shop/warenkorb.html")
+			data, hdrs = self._getResponse(withHeaders = True)
+			for hdr, value in hdrs:
+				if hdr.lower().strip() == "set-cookie":
+					m = re.match(r'PHPSESSID=(\w+).*',
+						     value, re.DOTALL)
+					if m:
+						return m.group(1)
+			time.sleep(0.2)
+		raise self.Error("Failed to get Pollin session ID.")
+
+	def connect(self):
+		if not self.conn:
+			PriceFetcher.connect(self)
+			self.__sid = self.__getSessionId()
+
+	def disconnect(self):
+		if self.conn:
+			self.__sid = None
+			PriceFetcher.disconnect(self)
+
+	def getPrice(self, orderCode):
+		self.connect()
+
+		orderCode = orderCode.strip()
+		if not orderCode:
+			raise self.Error("Invalid empty Pollin order code.")
+		orderCode = orderCode.split("-")
+		if len(orderCode) == 1:
+			wkz, bestellnr = "", orderCode[0]
+		elif len(orderCode) == 2:
+			wkz, bestellnr = orderCode
+		else:
+			raise self.Error("Invalid Pollin part number format "
+					 "(must be  00-000 000  or  000 000)")
+		wkz = re.sub(r'\s', "", wkz)
+		bestellnr = re.sub(r'\s', "", bestellnr)
+
+		header = self._defaultHttpHeader.copy()
+		header["Cookie"] = "PHPSESSID=" + self.__sid + "; pollincookie=1"
+		header["Content-Type"] = "application/x-www-form-urlencoded"
+
+		# Add the item to the basket
+		body = urllib.parse.urlencode(
+			(("do_anzahl_0", "1"),
+			 ("do_wkz_0", wkz),
+			 ("do_bestellnr2_0", bestellnr)),
+			encoding = "UTF-8",
+			errors = "ignore")
+		header["Content-Length"] = str(len(body))
+		self._sendRequest("POST", "/shop/warenkorb.html", body, header)
+		basket = self._getResponse().decode("UTF-8", "ignore")
+
+		# Remove the item from the basket.
+		body = urllib.parse.urlencode(
+			(("remoteAction", "deleteRemote"),
+			 ("type", "basket"),
+			 ("items", "{'items': ['[{"
+			 	   "\"artnrKurz\":\"%s\","
+				   "\"menge\":\"1\","
+				   "\"selected\":true,"
+				   "\"itemRow\":\"0\""
+				   "}]']}" % bestellnr)),
+			encoding = "UTF-8",
+			errors = "ignore")
+		header["Content-Length"] = str(len(body))
+		self._sendRequest("POST", "/shop/ajax.html", body, header)
+		self._getResponse()
+
+		# Extract the price from the basket.
+		m = re.match(r'.*'
+			     r'<td class="articleAvailability">\s*'
+			     r'<img src="/shop/images/spacer.gif"\s+'
+			     r'alt="[\w\s&;äöü\-]+"\s*/>\s*'
+			     r'</td>\s*'
+			     r'<td>\s*'
+			     r'(\d+,\d+)\s+&euro;\s*'
+			     r'</td>\s*'
+			     r'.*',
+			     basket, re.DOTALL)
+		if not m:
+			return self.Result(orderCode = orderCode,
+					   status = self.Result.NOTFOUND)
+		price = m.group(1)
+		try:
+			price = float(price.replace(",", "."))
+			if price <= 0.0:
+				raise ValueError
+		except ValueError as e:
+			return self.Result(orderCode = orderCode,
+					   status = self.Result.NOTFOUND)
+
+		return self.Result(orderCode = orderCode,
+				   price = price)
+
+PollinPriceFetcher.register()

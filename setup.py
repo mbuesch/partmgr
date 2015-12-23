8fe816cbc2f68c24fb59ed2baabdebce08dd831c
Michael Buesch <m@bues.ch>
2015-12-23 21:43:12 +0100

    setup.py: Add cx_Freeze and py2exe support
    Signed-off-by: Michael Buesch <m@bues.ch>

--- setup.py
+++ setup.py
@@ -2,7 +2,40 @@
 
 from partmgr.core.version import VERSION_STRING
 from distutils.core import setup
+import sys
+try:
+	import py2exe
+except ImportError as e:
+	py2exe = None
+try:
+	if py2exe and "py2exe" in sys.argv:
+		raise ImportError
+	from cx_Freeze import setup, Executable
+	cx_Freeze = True
+except ImportError as e:
+	cx_Freeze = False
 
+
+freezeExecutables = [ ("partmgr-gui", None), ]
+extraKeywords = {}
+if py2exe:
+	extraKeywords["console"] = [ s for s, e in freezeExecutables ]
+if cx_Freeze:
+	executables = []
+	for script, exe in freezeExecutables:
+		if exe:
+			if os.name.lower() in ("nt", "ce"):
+				exe += ".exe"
+			executables.append(Executable(script = script,
+						      targetName = exe))
+		else:
+			executables.append(Executable(script = script))
+	extraKeywords["executables"] = executables
+	extraKeywords["options"] = {
+			"build_exe"     : {
+				"packages"      : [ "partmgr", ],
+			}
+		}
 
 setup(	name		= "partmgr",
 	version		= VERSION_STRING,
@@ -10,7 +43,7 @@ setup(	name		= "partmgr",
 	license		= "GNU General Public License v2 or later",
 	author		= "Michael Buesch",
 	author_email	= "m@bues.ch",
-	url		= "http://bues.ch/cms/hacking/...",
+	url		= "http://bues.ch/h/partmgr",
 	packages	= [ "partmgr",
 			    "partmgr/core",
 			    "partmgr/pricefetch",
@@ -21,4 +54,5 @@ setup(	name		= "partmgr",
 	classifiers	= [
 	],
 #	long_description = open("README.txt").read(),
+	**extraKeywords
 )
